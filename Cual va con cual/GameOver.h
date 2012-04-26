20c25fa741a7103ef03617e07eaa3661c0148782
Gilberto <ripflame@gmail.com>
2012-04-25 19:05:35 -0500

    Modificaciones en los niveles y agregada pantalla de game over

---
+++ Cual va con cual/GameOver.h
@@ -0,0 +1,16 @@
+//
+//  GameOver.h
+//  Cual va con cual
+//
+//  Created by Gilberto Leon on 25/04/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameOver : CCLayer {
+    
+}
+
+@end
---
+++ Cual va con cual/GameOver.m
@@ -0,0 +1,14 @@
+//
+//  GameOver.m
+//  Cual va con cual
+//
+//  Created by Gilberto Leon on 25/04/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameOver.h"
+
+
+@implementation GameOver
+
+@end
